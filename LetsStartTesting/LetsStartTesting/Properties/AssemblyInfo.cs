31e6ea4df7a101bcab4e4f00328009297e7406ce
Balint Nandor <balint.nandor@codespring.ro>
2018-02-15 10:41:43 +0200

    Adding first project

---
+++ LetsStartTesting/LetsStartTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LetsStartTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LetsStartTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4823a5d7-52a0-45ba-9bd1-62915789df95")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LetsStartTesting/LetsStartTesting/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace LetsStartTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.qtptutorial.net/automation-practice/");
+        }
+    }
+}
