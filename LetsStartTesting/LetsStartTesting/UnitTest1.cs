117e7cc71282bbef95bde19416e3578d1a5398d2
Balint Nandor <balint.nandor@codespring.ro>
2018-02-15 15:29:13 +0200

    Some tests for clicking

--- LetsStartTesting/LetsStartTesting/UnitTest1.cs
+++ LetsStartTesting/LetsStartTesting/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace LetsStartTesting
@@ -8,11 +9,109 @@ namespace LetsStartTesting
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ClickElelemntById()
         {
             var driver = new ChromeDriver();
 
             driver.Navigate().GoToUrl("https://www.qtptutorial.net/automation-practice/");
+
+            var elementById = driver.FindElement(By.Id("idExample"));
+
+            elementById.Click();
+
+            var header = driver.FindElement(By.CssSelector("h1.main_title"));
+
+            string headerText = header.Text.ToString();
+
+            string successText = "Button success";
+
+            Assert.AreEqual(headerText, successText );
+
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void ClickElementByClass()
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.qtptutorial.net/automation-practice/");
+
+            var elementByClass = driver.FindElement(By.ClassName("buttonClassExample"));
+
+            elementByClass.Click();
+
+            var header = driver.FindElement(By.CssSelector("h1.main_title"));
+
+            string headerText = header.Text.ToString();
+
+            string successText = "Button success";
+
+            Assert.AreEqual(headerText, successText);
+
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void ClickElementByName()
+        {
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.qtptutorial.net/automation-practice/");
+
+            var elementByName = driver.FindElement(By.Name("NameExample"));
+
+            elementByName.Click();
+
+            var header = driver.FindElement(By.CssSelector("h1.main_title"));
+
+            string headerText = header.Text.ToString();
+
+            string successText = "Button success";
+
+            Assert.AreEqual(headerText, successText);
+
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void ClickElementsAndExit()
+        {
+            var driver = new ChromeDriver();
+
+            string successText = "Button success";
+
+            driver.Navigate().GoToUrl("https://www.qtptutorial.net/automation-practice/");
+
+            var elementById = driver.FindElement(By.Id("idExample"));
+
+            elementById.Click();
+
+            var header = driver.FindElement(By.CssSelector("h1.main_title"));
+
+            Assert.IsTrue( header.Text.ToString().Equals(successText) );
+
+            driver.Navigate().Back();
+
+            var elementByClass = driver.FindElement(By.ClassName("buttonClassExample"));
+
+            elementByClass.Click();
+
+            header = driver.FindElement(By.CssSelector("h1.main_title"));
+
+            Assert.IsTrue(header.Text.ToString().Equals(successText));
+
+            driver.Navigate().Back();
+
+            var elementByName = driver.FindElement(By.Name("NameExample"));
+
+            elementByName.Click();
+
+            header = driver.FindElement(By.CssSelector("h1.main_title"));
+
+            Assert.IsTrue(header.Text.ToString().Equals(successText));
+
+            driver.Close();
         }
     }
 }
